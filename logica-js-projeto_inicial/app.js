2fde3691af9e3f090671323ed335bf9f5dd02ee7
Matheus <matheusn06leite@gmail.com>
2025-01-15 17:29:56 -0300

    Iniciando a criacao do jogo do numero secreto

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto!"); // aparecer uma mensagem na tela
+let numeroSecreto = 5; // criar uma variavel
+let chute = prompt("Escolha um número entre 1 e 10"); //exibi uma caixa de diálogo que solicita uma entrada do usuário
+
+if (chute == numeroSecreto) {
+  console.log("Isso ai, você descobriu o número secreto(5)"); //imprimir mensagens no console do navegador
+}
