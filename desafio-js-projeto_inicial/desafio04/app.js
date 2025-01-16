f820cd7a5b7f7268fc44cbb46ad1d4e8ea1a4598
Matheus <matheusn06leite@gmail.com>
2025-01-16 17:38:33 -0300

    Colocando para gerar um numero secreto aleatorio

---
+++ desafio-js-projeto_inicial/desafio04/app.js
@@ -0,0 +1,39 @@
+//////////////////////////////Desafio 1
+
+// let contador = 1;
+
+// while(contador<=10){
+//     alert(contador);
+//     contador++;
+// }
+
+
+//////////////////////////////Desafio 2
+
+// let contador = 10;
+
+// while(contador>=0){
+//     alert(contador);
+//     contador--;
+// }
+
+
+//////////////////////////////Desafio 3
+
+// let numero = prompt("Digite um numero: ");
+
+// while(numero>=0){
+//     alert(numero);
+//     numero--;
+// }
+
+//////////////////////////////Desafio 4
+
+let numero = prompt("Digite um numero: ");
+let contador = 0
+
+while(contador<=numero){
+    alert(contador);
+    contador++;
+}
+
