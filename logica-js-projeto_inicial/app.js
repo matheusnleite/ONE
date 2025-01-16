f173242a64d74bdfb6eab7c7f91709e0a63dd791
Matheus <matheusn06leite@gmail.com>
2025-01-16 11:03:21 -0300

    Adicionando o numero de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,15 +1,21 @@
 alert('Boas vindas ao jogo do número secreto!'); // aparecer uma mensagem na tela
 let numeroSecreto = 5; // criar uma variavel
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10'); //exibi uma caixa de diálogo que solicita uma entrada do usuário
+let chute;
+let tentativas = 1;
 
-//se o chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-  alert(`Isso ai, você descobriu o número secreto ${numeroSecreto}`); //imprimir o texto + o valor da variavel
-} else {
-  if(chute < numeroSecreto){
-    alert(`O número secreto é maior que ${chute}`);
-  }else{
-    alert(`O número secreto é menor que ${chute}`);
+//enquanto o chute nao for igual ao numero secreto
+while(chute != numeroSecreto){
+  chute = prompt('Escolha um número entre 1 e 10'); //exibi uma caixa de diálogo que solicita uma entrada do usuário
+  //se o chute for igual ao numero secreto
+  if (chute == numeroSecreto) {
+    alert(`Isso ai, você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); //imprimir o texto + o valor da variavel
+  } else {
+    if(chute < numeroSecreto){
+      alert(`O número secreto é maior que ${chute}`);
+    }else{
+      alert(`O número secreto é menor que ${chute}`);
+    }
+    tentativas++;
   }
 }
