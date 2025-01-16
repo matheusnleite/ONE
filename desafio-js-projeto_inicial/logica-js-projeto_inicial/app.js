a2c7c4111a265400863e3b63b186f62221dbcf1c
Matheus <matheusn06leite@gmail.com>
2025-01-16 09:57:36 -0300

    Realizado o desafio inicial

---
+++ desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,14 @@
+alert("Boas Vindas ao nosso site!"); //desafio 1
+let nome = "Lua"; //desafio 2
+let idade = 25; //desafio 3
+let numeroDeVendas = 50; //desafio 4
+let saldoDisponivel = 1000; //desafio 5
+alert("Erro! Preencha todos os campos"); //desafio 6
+let mensagemDeErro = "Erro! Preencha todos os campos"; //desafio 7
+alert(mensagemDeErro); //desafio 7
+nome = prompt("Qual o seu nome?"); //desafio 8
+idade = prompt("Qual a sua idade?"); //desafio 9
+
+if (idade >= 18) {
+  alert("Pode tirar a habilitação");
+}
