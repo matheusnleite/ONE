c04be7c1db8bcca2d70b976285221ecaabc4b701
Matheus <matheusn06leite@gmail.com>
2025-01-16 15:58:02 -0300

    Adicionando operador ternario

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -9,7 +9,7 @@ while(chute != numeroSecreto){
   chute = prompt('Escolha um número entre 1 e 10'); //exibi uma caixa de diálogo que solicita uma entrada do usuário
   //se o chute for igual ao numero secreto
   if (chute == numeroSecreto) {
-    alert(`Isso ai, você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); //imprimir o texto + o valor da variavel
+    break;
   } else {
     if(chute < numeroSecreto){
       alert(`O número secreto é maior que ${chute}`);
@@ -19,3 +19,14 @@ while(chute != numeroSecreto){
     tentativas++;
   }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' // operador ternario que verifica se tentativa é maior que 1 e faz a mesma coisa que os ifs abaixo
+
+alert(`Isso ai, você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);//imprimir o texto + o valor da variavel
+
+// if(tentativas>1)
+// {  
+//   alert(`Isso ai, você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);//imprimir o texto + o valor da variavel
+// } else{
+//   alert(`Isso ai, você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
