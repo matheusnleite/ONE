77fb8857bb38a05e98151398bdb539890f626d0d
Matheus <matheusn06leite@gmail.com>
2025-01-16 10:18:44 -0300

    Aprendendo uma maneira de imprimir texto + variavel

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,10 @@
 alert('Boas vindas ao jogo do número secreto!'); // aparecer uma mensagem na tela
-let numeroSecreto = 5; // criar uma variavel
+let numeroSecreto = 8; // criar uma variavel
 console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10'); //exibi uma caixa de diálogo que solicita uma entrada do usuário
 
 if (chute == numeroSecreto) {
-  alert('Isso ai, você descobriu o número secreto(5)'); //imprimir mensagens no console do navegador
+  alert(`Isso ai, você descobriu o número secreto ${numeroSecreto}`); //imprimir o texto + o valor da variavel
 } else {
   alert('Você errou :(');
 }
