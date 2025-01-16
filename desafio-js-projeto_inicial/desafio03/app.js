e012505d2c5f76da9c6db705bd2c3d79b76c1cc7
Matheus <matheusn06leite@gmail.com>
2025-01-16 11:03:39 -0300

    Iniciando desafio 03

---
+++ desafio-js-projeto_inicial/desafio03/app.js
@@ -0,0 +1,40 @@
+//////////////////////////////Desafio 1
+
+// dia = prompt('Qual dia da semana é hoje?');
+
+// if(dia == 'Sábado' || dia == 'Domingo'){
+//   alert('Bom fim de semana!');
+// }else{
+//   alert('Boa semana!');
+// }
+
+//////////////////////////////Desafio 2
+
+// numero = prompt('Digite um número:');
+
+// if(numero < 0){
+//   alert('O número digitado é negativo!');
+// }else{
+//   alert('O número digitado é positivo!');
+// }
+
+//////////////////////////////Desafio 3
+
+// pontos = prompt('Digite a quantidade de pontos: ');
+
+// if(pontos >= 100){
+//   alert('Parabéns, você venceu!');
+// }else{
+//   alert('Tente novamente para ganhar.');
+// }
+
+//////////////////////////////Desafio 4
+
+// saldo = 1000.00
+
+// alert(`Seu saldo atual é: R$${saldo},00`);
+
+//////////////////////////////Desafio 5
+
+// nome = prompt('Qual o seu nome?');
+// alert(`Bem vindo ${nome}!`);
