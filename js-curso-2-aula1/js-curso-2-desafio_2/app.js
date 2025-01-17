6754d9fb20edcb6e276337bcbda52b84b23f48c3
Matheus <matheusn06leite@gmail.com>
2025-01-17 11:12:51 -0300

    Realizado desafio 2

---
+++ js-curso-2-aula1/js-curso-2-desafio_2/app.js
@@ -0,0 +1,42 @@
+//////////////////////////////Desafio 1
+// function olaMundo(){
+//     console.log('Olá, Mundo!');
+// }
+// olaMundo();
+
+//////////////////////////////Desafio 2
+// let nome = 'Matheus';
+// function cumprimentar(nome){
+//     console.log(`Olá, ${nome}`);
+// }
+// cumprimentar(nome);
+
+//////////////////////////////Desafio 3
+// let numero = 2;
+// function dobro(numero){
+//     return numero * 2;
+// }
+// console.log(dobro(numero));
+
+//////////////////////////////Desafio 4
+// let n1=4,n2=7,n3=5;
+// function calculaMedia(n1,n2,n3){
+//     let media = (n1+n2+n3)/3;
+//     return parseFloat(media.toFixed(1)); //retorna a media com apenas uma casa apos a virgula. OBS: o toFixed retorna uma string, se precisar do valor em numero, usar o parseFloat para converter
+// }
+// console.log(calculaMedia(n1,n2,n3));
+
+//////////////////////////////Desafio 5
+// let n1=5,n2=3;
+// function compara(n1,n2){
+//     let maior = n1>n2 ? n1:n2;
+//     return maior;
+// }
+// console.log(compara(n1,n2));
+
+//////////////////////////////Desafio 6
+let numero = 6;
+function multiplica(numero){
+    return numero * numero;
+}
+console.log(multiplica(numero));
