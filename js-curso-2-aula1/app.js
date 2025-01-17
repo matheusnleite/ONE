69a3f12f678dff6dd16b93ae6fe377169a1affa3
Matheus <matheusn06leite@gmail.com>
2025-01-17 16:05:10 -0300

    Habilitando o botao de reiniciar o jogo

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -12,8 +12,12 @@ function exibirTextoNaTela(tag, texto){ // funcao para otimizar o codigo, resumi
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');  //chamando a funcao de exibir texto na tela
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');  //chamando a funcao de exibir texto na tela
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+exibirMensagemInicial();
 
 function verificarChute(){ //funcao do botao de CHUTE
     let chute = document.querySelector('input').value; // pegar o valor dentro da tag
@@ -22,6 +26,7 @@ function verificarChute(){ //funcao do botao de CHUTE
         let palavraTentativa = tentativas > 1? 'tentativas':'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled'); // vai buscar o elemento pelo id (getElementById) e remover o atributo (removeAttribute) que passarmos
     }else{ 
         if(chute > numeroSecreto){
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -29,9 +34,23 @@ function verificarChute(){ //funcao do botao de CHUTE
                 exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++;
+        limparCampo();
     }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = ''; //colocando valor vazio no campo input
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);// colocando o atributo disabled novamente no botao
 }
