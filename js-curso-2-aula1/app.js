b658a364e93d99334d0b602ce06bb08271245f7c
Matheus <matheusn06leite@gmail.com>
2025-01-17 15:49:06 -0300

    Verificando o chute e contando tentativas

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -5,6 +5,7 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){ // funcao para otimizar o codigo, resumindo o que as 4 linhas acima faziam
     let campo = document.querySelector(tag);
@@ -16,7 +17,19 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){ //funcao do botao de CHUTE
     let chute = document.querySelector('input').value; // pegar o valor dentro da tag
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1? 'tentativas':'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    }else{ 
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+            }else{
+                exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+    }
 }
 
 function gerarNumeroAleatorio() {
