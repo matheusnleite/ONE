8dfae5bacfd0ac74cb00292a2612d72ae6bda2ae
Matheus <matheusn06leite@gmail.com>
2025-01-17 10:05:24 -0300

    Adicionando uma funcao de teste para quando o usuario clicar no botao CHUTE

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo do número secreto'; //Inserir o texto dentro da tag h1
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute(){ //funcao do botao de CHUTE
+    console.log('O botao foi clicado');
+}
