e9445b8aadf67fdb3120773cb4a6ef897a1512d1
Matheus <matheusn06leite@gmail.com>
2025-01-17 09:42:30 -0300

    Realizado o desafio 04

--- desafio-js-projeto_inicial/desafio04/app.js
+++ desafio-js-projeto_inicial/desafio04/app.js
@@ -1,39 +1,91 @@
 //////////////////////////////Desafio 1
 
-// let contador = 1;
-
-// while(contador<=10){
-//     alert(contador);
-//     contador++;
-// }
+// console.log('Bem vindo!');
 
 
 //////////////////////////////Desafio 2
 
-// let contador = 10;
-
-// while(contador>=0){
-//     alert(contador);
-//     contador--;
-// }
+// let nome = 'Matheus';
+// console.log(`Olá, ${nome}`);
 
 
 //////////////////////////////Desafio 3
 
-// let numero = prompt("Digite um numero: ");
-
-// while(numero>=0){
-//     alert(numero);
-//     numero--;
-// }
+// let nome = 'Matheus';
+// alert(`Olá, ${nome}`);
 
 //////////////////////////////Desafio 4
 
-let numero = prompt("Digite um numero: ");
-let contador = 0
+// let pergunta = prompt('Qual a linguagem de programação que você mais gosta?');
+// console.log(`Resposta: ${pergunta}`);
 
-while(contador<=numero){
-    alert(contador);
-    contador++;
-}
+//////////////////////////////Desafio 5
 
+// let valor1=7, valor2=5;
+// let resultado = valor1 + valor2;
+
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+//////////////////////////////Desafio 6
+
+// let valor1=7, valor2=5;
+// let resultado = valor1 - valor2;
+
+// console.log(`A diferença de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+
+//////////////////////////////Desafio 7
+
+// let idade = prompt('Qual a sua idade?');
+
+// if(idade >= 18){
+//     console.log('Maior de idade');
+// }else{
+//     console.log('Menor de idade');
+// }
+
+//////////////////////////////Desafio 8
+
+// let numero = prompt('Digite um número: ');
+
+// if(numero > 0){
+//     alert('Positivo');
+// }else if(numero == 0){
+//     alert('Zero');
+// }else {
+//     alert('Negativo');
+// }
+
+//////////////////////////////Desafio 9
+
+// let contador = 1;
+
+// while(contador<=10){
+//     console.log(contador);
+//     contador++;
+// }
+
+//////////////////////////////Desafio 10
+
+// let nota = 5;
+
+// if(nota >=7){
+//     console.log('Aprovado');
+// }else{
+//     console.log('Reprovado');
+// }
+
+//////////////////////////////Desafio 11
+
+// let numero = Math.random();
+// console.log(numero);
+
+//////////////////////////////Desafio 12
+
+// let numero = parseInt(Math.random() * 10 + 1);
+// console.log(numero);
+
+//////////////////////////////Desafio 13
+
+// let numero = parseInt(Math.random() * 1000 + 1);
+// console.log(numero);
